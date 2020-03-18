6856b68d20b80b03fd4484e570e07d4dd90dca9e
LPJworkroom <lpjworkroom@gmail.com>
2020-03-18 22:49:18 +0800

    Update Apriori.py

--- dataMining/Apriori.py
+++ dataMining/Apriori.py
@@ -1,10 +1,14 @@
 # Apriori.py by lpjworkroom
-# naive sample of apriori algorithm
+# naive apriori algorithm
+#
 # step:
-#   1 get minimun support threshold
-#   2 while item sets not empty:
+#   1. get minimun support threshold
+#   2. while item sets not empty:
 #       count items support
 #       remove items less than threshold
+
+from itertools import combinations
+
 
 class ItemSet(list):
     occurence=0 # number of set's occurence
@@ -12,13 +16,18 @@ class ItemSet(list):
 class Apriori:
     minSup=0 # minimun support threshold
     items=[] # store item sets
+
+    def __init__(self):
+        # initiat items and item occurence,to be done...
+        pass
     
     def solve(self):
-        items=list(self.items)
+        items=self.items.copy()
         k=1 # k th item set
         while True:
-            # get candidate item set with length k from items
-            candi=self.getCandidateSets(items,k)
+            candi=self.getCandidateSets(items,k) # get candidate item set with length k from items
+            if count(candi)==0: # k bigger than elements count,no combination
+                return items
             freq=[] # frequent sets for next loop
             for itemset in candi:
                 if self.itemsetOccurence(itemset,self.items)>=self.minSup:
@@ -30,17 +39,17 @@ class Apriori:
             k+=1
 
     def getCandidateSets(items,lenth):
-        # generate all itemsets of lenth from elements in all items' sets
+        # generate all combinations with lenth from elements in all itemsets
         ele=set()
-        for itemset in items:
+        for itemset in items: # unify elements
             for element in itemset:
                 ele.add(element)
-        return combination(list(ele),)
+        return [combinations([ele],lenth)]
 
     def itemsetOccurence(target,records):
         for itemset in records:
             occur=True
-            for ele in target:
+            for ele in target: # check if all element in target occurs
                 if itemset.count(ele)==0:
                     occur=False
                     break
